d42296e800be073b458b74fb270a490de5b1c973
J.D. Lowe <jdlowe.dev@gmail.com>
2013-11-22 00:51:30 -0500

    Past Goals scroll bar resolved!
    Hooray!

--- scripts/game.js
+++ scripts/game.js
@@ -1026,7 +1026,12 @@ Game.resizer = function() {
 	for(var i in lists) {
 		var currList = lists[i].join(",");
 		$(currList).each(function() {
-			$(this).css("height", ($(i).parent().height() - $(i).offset().top + $("#navbar").height()) + "px");
+			if($(this).attr("id") === "pastGoals") {
+				console.log($(this).parent().height() + " " + $("#game").height());
+				$(this).css("height", ($(this).parent().height() - $(i).offset().top - $("#navbar").height()) + "px");
+			}
+			else $(this).css("height", ($("#game").height() - $(i).offset().top - $("#navbar").height()) + "px");
+			//$(this).css("height", ($(i).parent().height() - $(i).offset().top + $("#navbar").height()) + "px");
 		});
 	}
 }
