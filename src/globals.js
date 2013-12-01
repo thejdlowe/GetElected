96c9a0b34dc1e9c45476e4c72c71d8b3dba2e944
J.D. Lowe <jedidrunkenllama@gmail.com>
2013-12-01 17:11:47 -0500

    Updating version number
    That's honestly it. Sheesh. Such a waste of commit, but have to keep the
    computers in sync, eh?

--- src/globals.js
+++ src/globals.js
@@ -1,4 +1,4 @@
-var version = "0.0.2";
+var version = "0.0.3";
 var currNotes = "";
 var efforttally = paperworktally = yessirtally = currentGoalIndex = 0;
 var scrollerBase = 10;	//An arbitrary number; this is where the (hidden) scroll bar will lock itself on scroll for the Yes Sir section.
