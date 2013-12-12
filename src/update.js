7a05b638caaac2e51431870167e673186504446e
J.D. Lowe <jdlowe.dev@gmail.com>
2013-12-11 22:19:15 -0500

    Restart bonus rebalanced to 300%
    Restart bonus is now calculated as
    
    % = (x / (250 + x)) * 3
    
    So if the player had a reset number of 100, that would be:
    
    (100 / 350) * 3, which would be 85.7% bonus

--- src/update.js
+++ src/update.js
@@ -1,6 +1,6 @@
 var calcBonus = function() {	//This...this is such a simple thing, but I didn't think of it. Thanks, Siveran!
-	var cap = 1;	//cap = 1 = 100%; the most is a 100% boost right now. The higher the cap, the more it's possible for players to boost. Later, I might raise this.
-	return (totalRestart / (100 + totalRestart)) * cap;
+	var cap = 3;	//cap = 1 = 100%; the most is a 100% boost right now. The higher the cap, the more it's possible for players to boost. Later, I might raise this.
+	return (totalRestart / (250 + totalRestart)) * cap;
 }
 
 var Update = function() {
